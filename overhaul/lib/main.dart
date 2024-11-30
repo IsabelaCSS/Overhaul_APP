feece6f9f76afcb0345c64d7d00ae785fd26141e
IsabelaCSS <isabela.oa2007@gmail.com>
2024-11-29 23:48:51 -0300

    Arrumado

--- overhaul/lib/main.dart
+++ overhaul/lib/main.dart
@@ -1,4 +1,4 @@
- // ignore_for_file: library_private_types_in_public_api, use_key_in_widget_constructors
+ // ignore_for_file: library_private_types_in_public_api, use_key_in_widget_constructors, prefer_const_constructors
 
   import 'package:flutter/material.dart';
   import 'DetalhesCarro.dart';
